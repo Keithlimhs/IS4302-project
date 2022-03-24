05837052fc0657cb6b4c42fcc5927d2105bfe89a
Khoo Wu Chuan <wuchuank@gmail.com>
2022-03-24 16:37:39 +0800

    Further tests on smart contract
    To-do:
    Yet to test the status modifiers validLeaveStatus()
    validLeaveId() <- not sure if this is needed cause we are passing in a leave instead of a leaveId

--- contracts/ClearLeth.sol
+++ contracts/ClearLeth.sol
@@ -2,220 +2,276 @@
 pragma solidity >=0.4.22 <0.9.0;
 
 contract ClearLeth {
-  address[] public registeredAddresses;
-  mapping(address => bool) employers;
-  mapping(address => bool) employees;
-  mapping(address => address) employeeToEmployer;
-  mapping(address => address[]) employerToEmployee;
-  uint public numOfEmployees = 0;
-  uint public numOfEmployers = 0;
+    address[] public registeredAddresses;
+    mapping(address => bool) employers;
+    mapping(address => bool) employees;
+    mapping(address => address) employeeToEmployer;
+    mapping(address => address[]) employerToEmployee;
+    uint256 public numOfEmployees = 0;
+    uint256 public numOfEmployers = 0;
 
-  enum leaveStatus { pending, cancelled, approved, rejected }
-  struct leave {
-    uint id;
-    string date; //can look into using timestamp
-    string reason;
-    leaveStatus status;
-    address employee;
-  }
-
-  uint public leaveAmount;
-  uint public numLeaves = 0;
-  leave[] allLeaves;
-  mapping(address => leave[]) employeeToLeaves;
-  mapping(address => uint) employeeLeaveBalance;
-  mapping(string => uint) datesToEmployeesApplied;
-  mapping(address => uint) employersToLeaveLimit;
-
-  event LeaveApplied(address employee, string date);
-  event LeaveApproved(address employee, address employer, uint leaveId);
-  event LeaveCancelled(address employee, uint leaveId);
-  event LeaveRejected(address employee, address employer, uint leaveId);
-  event LeaveLimitSet(address employer, uint limit);
-
-  // @dev Owner of contract will be able to add employers and is an employer
-  // @dev leaveAmount is also set when deployed
-  constructor(uint _leaveAmount) {
-    registeredAddresses.push(msg.sender);
-    numOfEmployers++;
-    employers[msg.sender] = true;
-    leaveAmount = _leaveAmount;
-  }
-
-  modifier validLeaveId(uint leaveId) {
-    require(leaveId < numLeaves, "Not Valid Leave Id");
-    _;
-  }
-
-  modifier employerOnly(address employeeAddress) {
-    require(employeeToEmployer[employeeAddress] == msg.sender, "Only employer of this employee can call this function");
-    _;
-  }
-
-  modifier isEmployer() {
-    require(employers[msg.sender] == true, "Only an employer can call this function");
-    _;
-  }
-
-  modifier validLeaveStatus(leave memory leaveApplication) {
-    require(leaveApplication.status == leaveStatus.pending, "Leave is not valid");
-    _;
-  }
-
-  function addEmployer(address _employerAddress) public isEmployer() {
-    registeredAddresses.push(_employerAddress);
-    employers[_employerAddress] = true;
-    numOfEmployers++;
-  }
-
-  function addEmployee(address _employeeAddress) public isEmployer() {
-    registeredAddresses.push(_employeeAddress);
-    employees[_employeeAddress] = true;
-    employerToEmployee[msg.sender].push(_employeeAddress);
-    employeeToEmployer[_employeeAddress] = msg.sender;
-    employeeLeaveBalance[_employeeAddress] = leaveAmount;
-    numOfEmployees++;
-  }
-
-  // @dev would be better if we just update mapping employees to false 
-  // then when we do getEmployees if wont appear
-  function removeEmployee(address _employeeAddress) public employerOnly(_employeeAddress) {
-    for (uint i = 0; i < registeredAddresses.length; i++) {
-      if (registeredAddresses[i] == _employeeAddress) {
-        registeredAddresses[i] = registeredAddresses[registeredAddresses.length-1];
-        registeredAddresses.pop();
-      }
+    enum leaveStatus {
+        pending,
+        cancelled,
+        approved,
+        rejected
+    }
+    struct leave {
+        uint256 id;
+        string date; //can look into using timestamp
+        bytes32 reason;
+        leaveStatus status;
+        address employee;
     }
 
-    for (uint j = 0; j < employerToEmployee[msg.sender].length; j++) {
-      if (employerToEmployee[msg.sender][j] == _employeeAddress) {
-        employerToEmployee[msg.sender][j] = employerToEmployee[msg.sender][employerToEmployee[msg.sender].length - 1];
-        employerToEmployee[msg.sender].pop();
-      }
+    uint256 public leaveAmount;
+    uint256 public numLeaves = 0;
+    leave[] allLeaves;
+    mapping(address => leave[]) employeeToLeaves;
+    mapping(address => uint256) employeeLeaveBalance;
+    mapping(string => uint256) datesToEmployeesApplied;
+    mapping(address => uint256) employersToLeaveLimit;
+
+    event LeaveApplied(address employee, string date);
+    event LeaveApproved(address employee, address employer, uint256 leaveId);
+    event LeaveCancelled(address employee, uint256 leaveId);
+    event LeaveRejected(address employee, address employer, uint256 leaveId);
+    event LeaveLimitSet(address employer, uint256 limit);
+
+    // @dev Owner of contract will be able to add employers and is an employer
+    // @dev leaveAmount is also set when deployed
+    constructor(uint256 _leaveAmount) {
+        registeredAddresses.push(msg.sender);
+        numOfEmployers++;
+        employers[msg.sender] = true;
+        leaveAmount = _leaveAmount;
     }
 
-    delete employeeToEmployer[_employeeAddress];
-    delete employeeToLeaves[_employeeAddress];
-    delete employeeLeaveBalance[_employeeAddress];
-  }
-
-  function getEmployeesByEmployer(address _employerAddress) public view returns (address[] memory) {
-    return employerToEmployee[_employerAddress];
-  }
-
-  // @dev transfer complexity to view function no gas 
-  function getAllEmployees() public view returns (address[] memory) {
-    address[] memory employeeArray = new address[](numOfEmployees);
-    uint counter = 0;
-    for (uint i = 0; i < registeredAddresses.length; i++) {
-      if (employees[registeredAddresses[i]] == true) {
-        employeeArray[counter] = registeredAddresses[i];
-        counter++;
-      }
-    }
-    return employeeArray;
-  }
-
-  // @dev transfer complexity to view function no gas 
-  function getAllEmployers() public view returns (address[] memory) {
-    address[] memory employerArray = new address[](numOfEmployers);
-    uint counter = 0;
-    for (uint i = 0; i < registeredAddresses.length; i++) {
-      if (employers[registeredAddresses[i]] == true) {
-        employerArray[counter] = registeredAddresses[i];
-        counter++;
-      }
-    }
-    return employerArray;
-  }
-
-  function applyLeave(string memory date, string memory reason) public {
-    leave memory newLeave = leave(
-      numLeaves,
-      date,
-      reason,
-      leaveStatus.pending,
-      msg.sender
-    );
-
-    numLeaves += 1;
-    employeeToLeaves[msg.sender].push(newLeave);
-    allLeaves.push(newLeave);
-    emit LeaveApplied(msg.sender, date);
-
-  }
-
-  function cancelLeave(leave memory leaveToCancel) public validLeaveStatus(leaveToCancel) {
-    for (uint i = 0; i < employeeToLeaves[msg.sender].length; i++) {
-      if (employeeToLeaves[msg.sender][i].id == leaveToCancel.id) {
-        employeeToLeaves[msg.sender][i].status = leaveStatus.cancelled;
-      }
-    }
-    emit LeaveCancelled(msg.sender, leaveToCancel.id);
-  }
-
-  function approveLeave(leave memory leaveToApprove) public validLeaveId(leaveToApprove.id) validLeaveStatus(leaveToApprove) employerOnly(leaveToApprove.employee) {
-    address employeeAddress = leaveToApprove.employee;
-    for (uint i = 0; i < employeeToLeaves[employeeAddress].length; i++) {
-      if (employeeToLeaves[employeeAddress][i].id == leaveToApprove.id) {
-        employeeToLeaves[employeeAddress][i].status = leaveStatus.approved;
-
-      }
+    modifier employerOnly(address employeeAddress) {
+        require(
+            employeeToEmployer[employeeAddress] == msg.sender,
+            "Only employer of this employee can call this function"
+        );
+        _;
     }
 
-    for (uint j = 0; j < allLeaves.length; j++) {
-      if (allLeaves[j].id == leaveToApprove.id) {
-        allLeaves[j].status = leaveStatus.approved;
-      }
+    modifier isEmployer() {
+        require(
+            employers[msg.sender] == true,
+            "Only an employer can call this function"
+        );
+        _;
     }
 
-    datesToEmployeesApplied[leaveToApprove.date]++;
-    employeeLeaveBalance[employeeAddress]--;
-    emit LeaveApproved(employeeAddress, msg.sender, leaveToApprove.id);
-  }
-
-  function rejectLeave(leave memory leaveToReject) public validLeaveId(leaveToReject.id) validLeaveStatus(leaveToReject) employerOnly(leaveToReject.employee) {
-    address employeeAddress = leaveToReject.employee;
-    for (uint i = 0; i < employeeToLeaves[employeeAddress].length; i++) {
-      if (employeeToLeaves[employeeAddress][i].id == leaveToReject.id) {
-        employeeToLeaves[employeeAddress][i].status = leaveStatus.rejected;
-
-      }
+    modifier validLeaveId(uint256 leaveId) {
+        require(leaveId < numLeaves, "Not Valid Leave Id");
+        _;
     }
 
-    for (uint j = 0; j < allLeaves.length; j++) {
-      if (allLeaves[j].id == leaveToReject.id) {
-        allLeaves[j].status = leaveStatus.rejected;
-      }
+    modifier validLeaveStatus(leave memory leaveApplication) {
+        require(
+            leaveApplication.status == leaveStatus.pending,
+            "Leave is not pending"
+        );
+        _;
     }
-    emit LeaveRejected(employeeAddress, msg.sender, leaveToReject.id);
-  }
 
-
-  function getLeaveBalance() public view returns (uint) {
-    return employeeLeaveBalance[msg.sender];
-  }
-
-  function getLeaveInformation(uint leaveId) public view returns (leave memory) {
-    for (uint i = 0; i < allLeaves.length; i++) {
-      if (allLeaves[i].id == leaveId) {
-        return allLeaves[i];
-      }
+    modifier isEmployeeOfLeave(leave memory _leave) {
+        require(
+            _leave.employee == msg.sender,
+            "Only employee of this leave can call this function"
+        );
+        _;
     }
-  }
 
-  function getLeaveApplications(address employeeAddress) public view returns (leave[] memory) {
-    return employeeToLeaves[employeeAddress];
-  }
+    modifier isEmployee() {
+        require(
+            employees[msg.sender] == true,
+            "Only employees can call this function"
+        );
+        _;
+    }
 
-  function setLeaveLimit(address employerAddress, uint limit) public {
+    function addEmployer(address _employerAddress) public isEmployer {
+        registeredAddresses.push(_employerAddress);
+        employers[_employerAddress] = true;
+        numOfEmployers++;
+    }
 
-    
-    emit LeaveLimitSet(employerAddress, limit);
-  }
+    function addEmployee(address _employeeAddress) public isEmployer {
+        registeredAddresses.push(_employeeAddress);
+        employees[_employeeAddress] = true;
+        employerToEmployee[msg.sender].push(_employeeAddress);
+        employeeToEmployer[_employeeAddress] = msg.sender;
+        employeeLeaveBalance[_employeeAddress] = leaveAmount;
+        numOfEmployees++;
+    }
 
+    // @dev would be better if we just update mapping employees to false
+    // then when we do getEmployees if wont appear
+    function removeEmployee(address _employeeAddress)
+        public
+        employerOnly(_employeeAddress)
+    {
+        for (uint256 i = 0; i < registeredAddresses.length; i++) {
+            if (registeredAddresses[i] == _employeeAddress) {
+                registeredAddresses[i] = registeredAddresses[
+                    registeredAddresses.length - 1
+                ];
+                registeredAddresses.pop();
+            }
+        }
 
+        for (uint256 j = 0; j < employerToEmployee[msg.sender].length; j++) {
+            if (employerToEmployee[msg.sender][j] == _employeeAddress) {
+                employerToEmployee[msg.sender][j] = employerToEmployee[
+                    msg.sender
+                ][employerToEmployee[msg.sender].length - 1];
+                employerToEmployee[msg.sender].pop();
+            }
+        }
 
+        delete employeeToEmployer[_employeeAddress];
+        delete employeeToLeaves[_employeeAddress];
+        delete employeeLeaveBalance[_employeeAddress];
+        numOfEmployees--;
+    }
 
+    function getEmployeesByEmployer(address _employerAddress)
+        public
+        view
+        returns (address[] memory)
+    {
+        return employerToEmployee[_employerAddress];
+    }
 
+    // @dev transfer complexity to view function no gas
+    function getAllEmployees() public view returns (address[] memory) {
+        address[] memory employeeArray = new address[](numOfEmployees);
+        uint256 counter = 0;
+        for (uint256 i = 0; i < registeredAddresses.length; i++) {
+            if (employees[registeredAddresses[i]] == true) {
+                employeeArray[counter] = registeredAddresses[i];
+                counter++;
+            }
+        }
+        return employeeArray;
+    }
+
+    // @dev transfer complexity to view function no gas
+    function getAllEmployers() public view returns (address[] memory) {
+        address[] memory employerArray = new address[](numOfEmployers);
+        uint256 counter = 0;
+        for (uint256 i = 0; i < registeredAddresses.length; i++) {
+            if (employers[registeredAddresses[i]] == true) {
+                employerArray[counter] = registeredAddresses[i];
+                counter++;
+            }
+        }
+        return employerArray;
+    }
+
+    function applyLeave(string memory date, bytes32 reason) public isEmployee {
+        leave memory newLeave = leave(
+            numLeaves,
+            date,
+            reason,
+            leaveStatus.pending,
+            msg.sender
+        );
+
+        numLeaves++;
+        employeeToLeaves[msg.sender].push(newLeave);
+        allLeaves.push(newLeave);
+        emit LeaveApplied(msg.sender, date);
+    }
+
+    function cancelLeave(leave memory _leave)
+        public
+        validLeaveStatus(_leave)
+        isEmployeeOfLeave(_leave)
+    {
+        for (uint256 i = 0; i < employeeToLeaves[msg.sender].length; i++) {
+            if (employeeToLeaves[msg.sender][i].id == _leave.id) {
+                employeeToLeaves[msg.sender][i].status = leaveStatus.cancelled;
+            }
+        }
+        emit LeaveCancelled(msg.sender, _leave.id);
+    }
+
+    function approveLeave(leave memory leaveToApprove)
+        public
+        validLeaveId(leaveToApprove.id)
+        validLeaveStatus(leaveToApprove)
+        employerOnly(leaveToApprove.employee)
+    {
+        address employeeAddress = leaveToApprove.employee;
+        for (uint256 i = 0; i < employeeToLeaves[employeeAddress].length; i++) {
+            if (employeeToLeaves[employeeAddress][i].id == leaveToApprove.id) {
+                employeeToLeaves[employeeAddress][i].status = leaveStatus
+                    .approved;
+            }
+        }
+
+        for (uint256 j = 0; j < allLeaves.length; j++) {
+            if (allLeaves[j].id == leaveToApprove.id) {
+                allLeaves[j].status = leaveStatus.approved;
+            }
+        }
+
+        datesToEmployeesApplied[leaveToApprove.date]++;
+        employeeLeaveBalance[employeeAddress]--;
+        emit LeaveApproved(employeeAddress, msg.sender, leaveToApprove.id);
+    }
+
+    function rejectLeave(leave memory leaveToReject)
+        public
+        validLeaveId(leaveToReject.id)
+        validLeaveStatus(leaveToReject)
+        employerOnly(leaveToReject.employee)
+    {
+        address employeeAddress = leaveToReject.employee;
+        for (uint256 i = 0; i < employeeToLeaves[employeeAddress].length; i++) {
+            if (employeeToLeaves[employeeAddress][i].id == leaveToReject.id) {
+                employeeToLeaves[employeeAddress][i].status = leaveStatus
+                    .rejected;
+            }
+        }
+
+        for (uint256 j = 0; j < allLeaves.length; j++) {
+            if (allLeaves[j].id == leaveToReject.id) {
+                allLeaves[j].status = leaveStatus.rejected;
+            }
+        }
+        emit LeaveRejected(employeeAddress, msg.sender, leaveToReject.id);
+    }
+
+    function getLeaveBalance() public view returns (uint256) {
+        return employeeLeaveBalance[msg.sender];
+    }
+
+    function getLeaveInformation(uint256 leaveId)
+        public
+        view
+        returns (leave memory)
+    {
+        for (uint256 i = 0; i < allLeaves.length; i++) {
+            if (allLeaves[i].id == leaveId) {
+                return allLeaves[i];
+            }
+        }
+        revert("Leave not found");
+    }
+
+    function getLeaveApplications(address employeeAddress)
+        public
+        view
+        returns (leave[] memory)
+    {
+        return employeeToLeaves[employeeAddress];
+    }
+
+    function setLeaveLimit(address employerAddress, uint256 limit) public {
+        emit LeaveLimitSet(employerAddress, limit);
+    }
 }
--- test/ClearLeth.test.js
+++ test/ClearLeth.test.js
@@ -1,34 +1,161 @@
-const _deploy_contracts = require("../migrations/2_deploy_contracts")
+const _deploy_contracts = require("../migrations/2_deploy_contracts");
 const truffleAssert = require("truffle-assertions");
 const assert = require("assert");
 const ClearLeth = artifacts.require("./ClearLeth.sol");
 
 contract("ClearLeth", (accounts) => {
-  let [owner, employer, employee] = accounts;
+    let [owner, employer, employer2, employee, employee2] = accounts;
 
-  before(async() => {
-    let initialLeaveAmount = 12
-    clearLethInstance = await ClearLeth.deployed(initialLeaveAmount);
-  });
+    before(async () => {
+        let initialLeaveAmount = 12;
+        clearLethInstance = await ClearLeth.deployed(initialLeaveAmount);
+    });
 
-  describe("Employer", async() => {
     // Test Case 1: Contract correctly deployed (constructor runs well)
-    it("Test Contract Constructor", async() => {
-      let employers = await clearLethInstance.getAllEmployers();
-      assert.equal(employers[0], owner, "Owner of contract not an employer")
-      let leaveAmount = await clearLethInstance.leaveAmount()
-      assert.equal(leaveAmount, 12, "Leave amount not initialized correctly")
-    })
+    it("Test Contract Constructor", async () => {
+        let employers = await clearLethInstance.getAllEmployers();
+        assert.equal(employers[0], owner, "Owner of contract not an employer");
+        let leaveAmount = await clearLethInstance.leaveAmount();
+        assert.equal(leaveAmount, 12, "Leave amount not initialized correctly");
+    });
 
-    // Test Case 2: Add functions
-    it("Test Add functions", async() => {
-      await clearLethInstance.addEmployer(employer, {from: owner});
-      let employers = await clearLethInstance.getAllEmployers();
-      assert.equal(employers[1], employer, "Employer not added successfully");
+    // Test Case 2: Add/remove functions
+    it("Test Add and Remove functions", async () => {
+        await clearLethInstance.addEmployer(employer, { from: owner });
+        let employers = await clearLethInstance.getAllEmployers();
+        assert.equal(employers[1], employer, "Employer not added successfully");
 
-      await clearLethInstance.addEmployee(employee, {from: employer});
-      let employees = await clearLethInstance.getAllEmployees();
-      assert.equal(employees[0], employee, "Employee not added successfully");
-    })
-  })
+        await clearLethInstance.addEmployee(employee, { from: employer });
+        let employeesAdd = await clearLethInstance.getAllEmployees();
+        assert.equal(
+            employeesAdd[0],
+            employee,
+            "Employee not added successfully"
+        );
+
+        await clearLethInstance.removeEmployee(employee, { from: employer });
+        let employeesRemove = await clearLethInstance.getAllEmployees();
+        assert.equal(
+            employeesRemove.length,
+            0,
+            "Employee not removed successfuly"
+        );
+    });
+
+    // Test Case 2b: Modifers for add/remove functions
+    it("Ensure only employers can add or remove employers/employees", async () => {
+        await clearLethInstance.addEmployee(employee, { from: employer });
+        let employeesAdd = await clearLethInstance.getAllEmployees();
+        assert.equal(
+            employeesAdd[0],
+            employee,
+            "Employee not added successfully"
+        );
+
+        await truffleAssert.reverts(
+            clearLethInstance.addEmployee(employee2, { from: employee }),
+            "Only an employer can call this function"
+        );
+
+        await truffleAssert.reverts(
+            clearLethInstance.addEmployer(employer2, { from: employee }),
+            "Only an employer can call this function"
+        );
+
+        await clearLethInstance.addEmployer(employer2, { from: owner });
+        await truffleAssert.reverts(
+            clearLethInstance.removeEmployee(employee, { from: employer2 }),
+            "Only employer of this employee can call this function"
+        );
+    });
+
+    // Test Case 3: Applying/cancelling leaves
+    it("Test applying and cancelling of leaves", async () => {
+        let leaveApplication = await clearLethInstance.applyLeave(
+            "2022-03-05",
+            web3.utils.asciiToHex("test"),
+            { from: employee }
+        );
+        truffleAssert.eventEmitted(leaveApplication, "LeaveApplied");
+
+        let leave = await clearLethInstance.getLeaveInformation(0);
+        let leaveCancellation = await clearLethInstance.cancelLeave(leave, {
+            from: employee,
+        });
+        truffleAssert.eventEmitted(leaveCancellation, "LeaveCancelled");
+    });
+
+    // Test Case 3b: Testing reverts
+    it("Ensure only employees can apply leave & cancel leaves", async () => {
+        await truffleAssert.reverts(
+            clearLethInstance.applyLeave(
+                "2022-03-05",
+                web3.utils.asciiToHex("test"),
+                { from: employer }
+            ),
+            "Only employees can call this function"
+        );
+
+        let leaveApplication = await clearLethInstance.applyLeave(
+            "2022-03-05",
+            web3.utils.asciiToHex("test"),
+            { from: employee }
+        );
+        truffleAssert.eventEmitted(leaveApplication, "LeaveApplied");
+
+        let leave = await clearLethInstance.getLeaveInformation(1);
+        await truffleAssert.reverts(
+            clearLethInstance.cancelLeave(leave, {
+                from: employer,
+            }),
+            "Only employee of this leave can call this function"
+        );
+    });
+
+    // Test Case 4: Approving/rejecting leaves
+    it("Test approving and rejecting of leaves", async () => {
+        let leave = await clearLethInstance.getLeaveInformation(1);
+        let leaveApproval = await clearLethInstance.approveLeave(leave, {
+            from: employer,
+        });
+        truffleAssert.eventEmitted(leaveApproval, "LeaveApproved");
+
+        let leaveApplication2 = await clearLethInstance.applyLeave(
+            "2022-04-05",
+            web3.utils.asciiToHex("test"),
+            { from: employee }
+        );
+        truffleAssert.eventEmitted(leaveApplication2, "LeaveApplied");
+
+        let leave2 = await clearLethInstance.getLeaveInformation(2);
+        let leaveRejection = await clearLethInstance.rejectLeave(leave2, {
+            from: employer,
+        });
+        truffleAssert.eventEmitted(leaveRejection, "LeaveRejected");
+    });
+
+    // Test Case 4b: Testing modifiers
+    it("Ensure only employers of employee can approve or reject leave", async () => {
+        let leaveApplication2 = await clearLethInstance.applyLeave(
+            "2022-05-05",
+            web3.utils.asciiToHex("test"),
+            { from: employee }
+        );
+        truffleAssert.eventEmitted(leaveApplication2, "LeaveApplied");
+
+        let leave3 = await clearLethInstance.getLeaveInformation(3);
+        await truffleAssert.reverts(
+            clearLethInstance.approveLeave(leave3, {
+                from: employee,
+            }),
+            "Only employer of this employee can call this function"
+        );
+
+        await truffleAssert.reverts(
+            clearLethInstance.rejectLeave(leave3, {
+                from: employee,
+            }),
+            "Only employer of this employee can call this function"
+        );
+    });
 });
